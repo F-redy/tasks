5737e2547a5de9199329e82530d2f4c13dc327cd
Stanislav <nstanislass@gmail.com>
2023-03-05 22:01:23 +0200

    add function_running_time

---
+++ function_decorators/function_running_time.py
@@ -0,0 +1,13 @@
+from time import time
+from typing import Callable, Any
+
+
+def timer_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+        print(f"Время выполнения функции {func.__name__}: {end_time - start_time} секунд")
+        return result
+
+    return wrapper
