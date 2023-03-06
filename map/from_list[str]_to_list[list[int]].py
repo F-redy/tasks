65c6775e04e16064893e9517832c68010712c05f
Stanislav <nstanislass@gmail.com>
2023-03-06 13:53:03 +0200

    fix 25 line add func check_integers_only, instead of 2 duplicate lines

--- map/from_list[str]_to_list[list[int]].py
+++ map/from_list[str]_to_list[list[int]].py
@@ -12,12 +12,16 @@
 
 import sys
 
+
+def check_integers_only(lst: list[list]) -> bool:
+    return all([lambda line: all([lambda x: type(x) == int, line]), lst])
+
+
 lst_in = list(map(str.strip, sys.stdin.readlines()))  # остановка ввода ctrl + D
 
 lst2D_1 = [list(map(int, line.split())) for line in lst_in]
 lst2D_2 = list(map(lambda x: list(map(int, x.split())), lst_in))
 
-print(all([lambda line: all([lambda x: type(x) == int, line]), lst2D_1]), end=' ')
-print(all([lambda line: all([lambda x: type(x) == int, line]), lst2D_2]))
+print(*[check_integers_only(lst_x) for lst_x in [lst2D_1, lst2D_2]])
 
 [print(row1, row2) for row1, row2 in zip(lst2D_1, lst2D_2)]
