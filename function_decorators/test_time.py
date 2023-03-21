860410dac8a6153eb3a7daa1742f73c9ac43acbb
Stanislav <nstanislass@gmail.com>
2023-03-22 01:32:26 +0200

    fix annotation

--- function_decorators/test_time.py
+++ function_decorators/test_time.py
@@ -1,14 +1,18 @@
 from time import time
+from typing import Callable, Any
 
 __all__ = ('test_time',)
+
+
 # for import func: from function_decorators.test_time import test_time
 
-def test_time(func):
-    def wrapper(*args, **kwargs):
+def test_time(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         start = time()
         for _ in range(100000):
             _ = func(*args, **kwargs)
-        print(f'Результат работы функции {func.__name__}: {(time() - start):.2f} сек')
+        finish = time() - start
+        print(f'Результат работы функции {func.__name__}: {finish:.2f} сек')
         return func
 
     return wrapper
