6b37f4b25ffbc6d3f5b41a6d0c2f3b244022bcce
Stanislav <nstanislass@gmail.com>
2023-04-02 15:53:35 +0300

    add doc string

--- raise_module/valleys_count/check_valleys_in_path.py
+++ raise_module/valleys_count/check_valleys_in_path.py
@@ -8,21 +8,51 @@ TEXT_RESULT = (
 
 
 def check_length_path(all_steps: int, path: str) -> None:
+    """
+    Check if the number of steps matches the length of the path.
+
+    :param all_steps:  Total number of steps.
+    :param path: String representing the path.
+    :raises: ValueError: If the number of steps does not match the length of the path.
+    """
     if all_steps != len(path):
         raise ValueError('The number of steps is not equal to the path')
 
 
 def check_valid_string_type(path: str) -> None:
+    """
+    Check if the input path is a valid string type.
+
+    :param path: String representing the path.
+    :raises: TypeError: If the input path is not a string type.
+    """
+
     if not isinstance(path, str):
         raise TypeError('string must be only str.')
 
 
 def check_valid_path(path: str) -> None:
+    """
+    Check if the input path is a valid path.
+
+    :param path: String representing the path.
+    :raises: ValueError: If the input path contains characters other than 'U' and 'D'.
+    """
+
     if any(c not in VALID_CHARS for c in path):
         raise ValueError('path must be only a letters {} or {}.'.format(*VALID_CHARS))
 
 
 def check_path(all_steps: int, path: str, funcs_tuple: tuple) -> None:
+    """
+    Check if the input path is valid.
+
+    :param all_steps: Total number of steps.
+    :param path: String representing the path.
+    :param funcs_tuple: Tuple of functions used for checking the input path.
+    :raises: ValueError: If the input path is invalid.
+    """
+
     try:
         for check_func in funcs_tuple:
             if check_func.__name__ == 'check_length_path':
@@ -34,6 +64,13 @@ def check_path(all_steps: int, path: str, funcs_tuple: tuple) -> None:
 
 
 def print_valley_count_text(valley: int) -> None:
+    """
+    Print the output text based on the number of valleys.
+
+    :param valley: Number of valleys.
+    :return: None
+    """
+
     match valley:
         case 1:
             print(TEXT_RESULT[0].format(valley))
@@ -44,6 +81,14 @@ def print_valley_count_text(valley: int) -> None:
 
 
 def count_valleys_in_path(all_steps: int, path: str) -> int:
+    """
+    Count the number of valleys in the input path.
+
+    :param all_steps: Total number of steps.
+    :param path: String representing the path.
+    :return: Number of valleys in the input path.
+    """
+
     funcs_tuple = (check_valid_string_type, check_valid_path, check_length_path)
     check_path(all_steps, path, funcs_tuple)
 
