bb0816003cddb05cd4b0666362f2ebd0a20116c4
Stanislav <nstanislass@gmail.com>
2023-04-03 03:27:15 +0300

    fix annotation

--- libraries/numpy_project/diagonal_difference.py
+++ libraries/numpy_project/diagonal_difference.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-def find_diagonal_difference(nested_list):
+def get_diagonal_difference(nested_list: list[list[int]]) -> int:
     matrix = np.array(nested_list)
-    return np.abs(np.diag(matrix).sum() - np.diag(np.fliplr(matrix)).sum())
-
+    return np.abs(np.diag(matrix).sum() - np.diag(np.fliplr(matrix)).sum()).item()
