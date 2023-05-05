c906660c5eb564ea68e12989eff59676aa198585
Stanislav <nstanislass@gmail.com>
2023-05-05 14:08:51 +0300

    delete 52 line

--- matrix/filling_with_neighbors2.py
+++ matrix/filling_with_neighbors2.py
@@ -49,7 +49,7 @@
 n, m = map(int, input().split())
 
 matrix = [list(map(int, input().split())) for _ in range(n)]
-[print(i) for i in matrix]
+
 for i in range(n - 2, -1, -1):
     for j in range(m - 2, -1, -1):
         matrix[i][j] = matrix[i + 1][j] + matrix[i][j + 1]
