95952cb9119e9370d04e5e6feebe40bf2ea8a094
Stanislav <nstanislass@gmail.com>
2023-07-01 15:37:31 +0300

    add YouTube_v=.py

---
+++ regix_module/YouTube_v=.py
@@ -0,0 +1,50 @@
+# YT ID
+# Условие:
+# Получите все идентификаторы видеороликов на YouTube, используя регулярные выражения.
+#
+# Что нужно найти:
+# Нужно найти последовательности, подходящие по следующим условиям:
+#
+# Cостоит из символов латинского алфавита обоих регистров, цифр, а также _
+# Перед последовательностью стоит v=
+# Sample Input 1:
+#
+#
+# Sample Output 1:
+#
+#
+# Sample Input 2:
+#
+#
+# Sample Output 2:
+#
+#
+# Sample Input 3:
+#
+#
+# Sample Output 3:
+#
+#
+# Sample Input 4:
+#
+#
+# Sample Output 4:
+#
+#
+
+import re
+
+test_case = [
+    ('https://youtu.be/watch?v=dQw4w9WgXcQ&list=PLi9drqWffJ9FWBo7ZVOiaVy0UQQEm4IbP', 'dQw4w9WgXcQ'),
+    ('https://www.youtube.com/watch?v=jNQXAC9IVRw', 'jNQXAC9IVRw'),
+    ('https://m.youtube.com/watch?v=pXRviuL6vMY&list=PLrhpb4TQr-uKzxOB1C_9x-Ysrj2WRMZN_&index=16&t=535s',
+     'pXRviuL6vMY'),
+    ('https://www.youtube.com/watch?app=desktop&v=WUEVJ0N6I1A&t=1s', 'WUEVJ0N6I1A'),
+]
+
+pattern = r"(?<=v=)[A-z\d_]+"
+
+for i, (example, answer) in enumerate(test_case, 1):
+    result = ' '.join(re.findall(pattern, example))
+    assert result == answer, f'TEST {i} - ERROR:\n{result} != {answer}'
+    print(f'TEST {i} - OK')
