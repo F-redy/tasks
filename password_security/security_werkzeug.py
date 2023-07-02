58e244190fc0a3673a39442e0ddff0cbad65a44f
Stanislav <nstanislass@gmail.com>
2023-07-02 16:14:40 +0300

    add security_werkzeug.py.py

---
+++ password_security/security_werkzeug.py
@@ -0,0 +1,21 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from generation_random_password import generation_random_password
+
+
+def generation_random_hash_password(length_password: int):
+    random_password = generation_random_password(length_password=length_password)
+    hashed_password = generate_password_hash(random_password)
+    return hashed_password
+
+
+def hash_password(password: str) -> str:
+    return generate_password_hash(password)
+
+
+if __name__ == '__main__':
+    n = 33
+    random_psw = generation_random_password(length_password=n)
+    hashed_psw = generate_password_hash(random_psw)
+
+    print(check_password_hash(hashed_psw, random_psw))
