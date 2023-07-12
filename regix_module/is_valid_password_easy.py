c4fdd803924da618de8d978741493a20504b75e1
Stanislav <nstanislass@gmail.com>
2023-07-12 12:56:18 +0300

    add is_valid_password_easy.py

---
+++ regix_module/is_valid_password_easy.py
@@ -0,0 +1,37 @@
+# Проверка пароля
+# Условие:
+# Проверьте пароль на валидность.
+#
+# Что нужно сделать:
+# Проверить пароль на валидность. Валидным будем считать пароль, который:
+#
+# Состоит из a-z, A-Z, 0-9, @#$%^&*()_-+!?
+# Его длина минимум 8 символов
+# Тестовые данные:
+# Входные данные:
+# На вход программе подаётся 1 строка.
+#
+# Выходные данные:
+# Если это валидный пароль - выводите True, иначе - False.
+
+import re
+
+test_case = [
+    ('d2D8dh8DA#!', True),
+    ('g0JDQ0#(dka02b vp', False),
+    ('Это не пароль', False),
+    ('111111111$%^1111111111', True),
+    ('@#$%^64&*()_-+!?', True),
+]
+
+
+def is_valid_password(password: str) -> bool:
+    simbols = ''.join(map(re.escape, '@#$%^&*()_-+!?'))
+    pattern = re.compile(r"[A-Za-z\d{}]{}".format(simbols, '{8,}'))
+    return bool(pattern.fullmatch(password))
+
+
+for i, (test, result) in enumerate(test_case, 1):
+    res = is_valid_password(test)
+    assert res == result, f'TEST №{i}: ERROR!\n{res} != {result}'
+    print(f'TEST №{i}: OK')
