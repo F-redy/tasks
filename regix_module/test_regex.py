8f0aded731b2c4636fd8d65928779a9ad7ca6b37
Stanislav <nstanislass@gmail.com>
2023-07-30 11:46:59 +0300

    add param method, add regex_method, fix 17 line

--- regix_module/test_regex.py
+++ regix_module/test_regex.py
@@ -1,17 +1,19 @@
 import re
 
 
-def test_regex(test_case: list[tuple[str, str]], regex: str) -> None:
+def test_regex(test_case: list[any], pattern: str, method: str) -> None:
     """
     Tests a regular expression against a list of test cases.
 
     :param test_case: A list of test cases, where each test case is represented as a tuple containing an example string
      and the expected answer.
-    :param regex: The regular expression pattern to be tested.
+    :param pattern: The regular expression pattern to be tested.
+    :param method: The name of the regular expression method to be used ('search', 'findall', 'finditer', etc.).
     :return:This function does not return anything. It raises an AssertionError if any of the test cases fail.
     :raise:If the result of the regular expression does not match the expected answer for any of the test cases.
     """
-    for i, (example, answer) in enumerate(test_case, 1):
-        result = ' '.join(re.findall(regex, example))
-        assert result == answer, f'TEST №{i} - ERROR!\n{result} != {answer}'
+    for i, (test, answer) in enumerate(test_case, 1):
+        regex_method = getattr(re, method)
+        result = regex_method(pattern, test)
+        assert result == answer, f'TEST №{i} - ERROR!\n{result} != {answer}\nError in {test}'
         print(f'TEST №{i} - OK!')
