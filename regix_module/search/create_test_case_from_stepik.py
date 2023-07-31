7f54effcf508de0811a69037aa7d0faddde379a7
Stanislav <nstanislass@gmail.com>
2023-07-31 15:52:13 +0300

    fix doc, delete func param, add sys.stdin.readlines

--- regix_module/search/create_test_case_from_stepik.py
+++ regix_module/search/create_test_case_from_stepik.py
@@ -2,26 +2,28 @@ import re
 import sys
 
 
-def create_test_case(text: list[str]) -> list[str]:
+def create_test_case() -> list[str]:
     """
     Creates a test dataset from text - https://stepik.org/.
 
-    :param text: List of lines of text containing test data.
     :return: A list of strings representing the test data set.
     """
+    length_line = "-" * 158
+    print(length_line, '\nEnter the text:')
+    # text_stepik: list[str] = list(map(str.strip, sys.stdin.readlines()))
+    text_stepik: list[str] = [line.strip() for line in sys.stdin.readlines() if line.strip()]
+
     test_case = []
-    for i, line in enumerate(text):
+    for i, line in enumerate(text_stepik):
         if re.search(r"Sample Input.+", line):
-            test_case.append(text[i + 2])
+            test = text_stepik[i + 1]
+            test_case.append(test)
+            print(test)
+
+    print(length_line, '\n', test_case)
+
     return test_case
 
 
 if __name__ == '__main__':
-    print('Enter the text:')
-    text_stepik = list(map(str.strip, sys.stdin.readlines()))
-    print("-" * 154)
-    result_test_case = create_test_case(text_stepik)
-    for test in result_test_case:
-        print(test)
-    print("-" * 154)
-    print(result_test_case)
+    result_test_case = create_test_case()
