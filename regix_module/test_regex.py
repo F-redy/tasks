ebd86309ad79ab2a86857e5b3147cf900213b134
Stanislav <nstanislass@gmail.com>
2023-08-01 16:19:31 +0300

    add param replace_group, flags, fix regex_method

--- regix_module/test_regex.py
+++ regix_module/test_regex.py
@@ -1,7 +1,7 @@
 import re
 
 
-def test_regex(test_case: list[any], pattern: str, method: str) -> None:
+def test_regex(test_case: list[any], pattern: str, method: str, replace_group: str = None, flags: int = 0) -> None:
     """
     Tests a regular expression against a list of test cases.
 
@@ -9,11 +9,16 @@ def test_regex(test_case: list[any], pattern: str, method: str) -> None:
      and the expected answer.
     :param pattern: The regular expression pattern to be tested.
     :param method: The name of the regular expression method to be used ('search', 'findall', 'finditer', etc.).
+    :param replace_group: Optional. The name of the capturing group.
+    :param flags: Optional. Flags that modify the behavior of the regular expression. Default is 0 (no flags).
     :return:This function does not return anything. It raises an AssertionError if any of the test cases fail.
     :raise:If the result of the regular expression does not match the expected answer for any of the test cases.
     """
     for i, (test, answer) in enumerate(test_case, 1):
         regex_method = getattr(re, method)
-        result = regex_method(pattern, test)
+        if replace_group:
+            result = regex_method(pattern, replace_group, test, flags=flags)
+        else:
+            result = regex_method(pattern, test, flags=flags)
         assert result == answer, f'TEST №{i} - ERROR!\n{result} != {answer}\nError in {test}'
-        print(f'TEST №{i} - OK!')
+        print(f'TEST №{i} - OK!\n{result}')
