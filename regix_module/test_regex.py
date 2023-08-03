b8fe89d5e4c89284d95b26669f8c8cf8cc3fc7bf
Stanislav <nstanislass@gmail.com>
2023-08-03 17:41:33 +0300

    add compile

--- regix_module/test_regex.py
+++ regix_module/test_regex.py
@@ -14,11 +14,14 @@ def test_regex(test_case: list[any], pattern: str, method: str, repl: [str, call
     :return:This function does not return anything. It raises an AssertionError if any of the test cases fail.
     :raise:If the result of the regular expression does not match the expected answer for any of the test cases.
     """
+    pattern = re.compile(pattern)
+    regex_method = getattr(re, method)
+
     for i, (test, answer) in enumerate(test_case, 1):
-        regex_method = getattr(re, method)
+
         if repl:
-            result = regex_method(pattern, repl, test, flags=flags)
+            result = pattern.regex_method(repl, test, flags=flags)
         else:
-            result = regex_method(pattern, test, flags=flags)
+            result = pattern.regex_method(test, flags=flags)
         assert result == answer, f'TEST №{i} - ERROR!\n{result} != {answer}\nERROR in:\n {test}'
         print(f'TEST №{i} - OK\n{result}\n')
