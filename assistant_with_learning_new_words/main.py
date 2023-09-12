d5c83de18ac0fc2d7a1b5184891f29be03a2c0c4
Stanislav <nstanislass@gmail.com>
2023-09-12 22:32:48 +0300

    add func paint, italics

--- assistant_with_learning_new_words/main.py
+++ assistant_with_learning_new_words/main.py
@@ -1,16 +1,39 @@
 from random import choice
 
 
+def paint(text: str, color: str) -> str:
+    """Colors: green, red, yellow, blue, violet, light_blue"""
+    colors = {
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'violet': '\033[35m',
+        'light_blue': '\033[36m',
+        'drop_color': '\033[0m'
+    }
+    try:
+        return f"{colors[color]}{text}{colors['drop_color']}"
+    except KeyError:
+        raise ValueError(f'Don\'t have color: {color}')
+
+
+def italics(text: str) -> str:
+    return f"\033[3m{text}\033[0m"
+
+
 def read_words(words: dict) -> dict:
-    print(f'{" ":<60} level: {stage}')
+    print(f'\n{" ":<60} {paint(f"level: {stage}", "violet")}\n')
     for (en, ru), point in words.items():
-        question = input(f'Enter translation for "{ru}": ').strip().lower()
+        question = input(f'{paint("Enter translation for", "light_blue")} "{italics(ru)}": ').strip().lower()
+
         if question == en.strip().lower():
             words[(en, ru)] += 1
-            print(f'Right! "{en}" - "{ru}" has point: {point + 1}\n')
+            print(f'{paint("Right!", "green")} "{en}" - "{ru}" {paint(f"has point: {point + 1}", "green")}\n')
         else:
-            print(f'Error! Point -1. "{en}" - "{ru}" has point: {point -1}\n')
+            print(f'{paint("Error!", "red")} "{en}" - "{ru}" {paint(f"has point: {point - 1}", "red")}\n')
             words[(en, ru)] -= 1
+
     return words
 
 
@@ -20,7 +43,7 @@ def check_point(words: dict) -> bool:
 
 def get_new_pair() -> tuple[str, str]:
     new_word = choice(some_words)
-    print(f'{" ":>55}new pair: "{" - ".join(new_word)}"')
+    print(f'{" ":>50}{paint("new pair:", "yellow")} "{italics(" - ".join(new_word))}"')
     return new_word
 
 
@@ -49,10 +72,14 @@ some_words = [
 words_to_learn = start_game()
 stage = 1
 
-
 while True:
-    if not check_point(read_words(words_to_learn)):
-        words_to_learn = read_words(words_to_learn)
-    if some_words:
-        stage += 1
-        words_to_learn.update({get_new_pair(): 0})
+    words_to_learn = read_words(words_to_learn)
+    check = check_point(words_to_learn)
+    match check:
+        case False:
+            words_to_learn = read_words(words_to_learn)
+        case True if some_words:
+            stage += 1
+            words_to_learn.update({get_new_pair(): 0})
+        case _:
+            break
