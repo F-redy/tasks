26195084db44745f5aba6eea179cd97ad2f1f70f
Stanislav <nstanislass@gmail.com>
2023-09-17 20:55:15 +0300

    add collections_Counter.py

---
+++ libraries/itertools _module/collections_Counter.py
@@ -0,0 +1,51 @@
+# A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+#
+# Task
+#
+# Raghu is a shoe shop owner. His shop has X number of shoes.
+# He has a list containing the size of each shoe he has in his shop.
+# There are N number of customers who are willing to pay Xi amount of money only if they get the shoe of their desired size.
+#
+# Your task is to compute how much money Raghu earned.
+#
+# Input Format
+#
+# The first line contains X, the number of shoes.
+# The second line contains the space separated list of all the shoe sizes in the shop.
+# The third line contains N, the number of customers.
+# The next N lines contain the space separated values of the shoe size desired by the customer and Xi,
+# the price of the shoe.
+#
+# Output Format
+#
+# Print the amount of money earned by Raghu.
+#
+# Sample Input
+#
+# 10
+# 2 3 4 5 6 8 7 6 5 18
+# 6
+# 6 55
+# 6 45
+# 6 55
+# 4 40
+# 18 60
+# 10 50
+
+# Sample Output
+#
+# 200
+
+from collections import Counter
+
+_ = int(input())
+size_shoes = Counter(map(int, input().split()))
+customers = int(input())
+total_check = 0
+for _ in range(customers):
+    size, price = map(int, input().split())
+    if size_shoes.get(size):
+        size_shoes[size] -= 1
+        total_check += price
+
+print(total_check)
