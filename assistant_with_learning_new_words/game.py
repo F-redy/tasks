47f6c9d93ad52990f91c7262361d39a3a287ceb3
Stanislav <nstanislass@gmail.com>
2023-09-20 00:50:37 +0300

    add class Printer:  - move function paint  - move function italics  - move function print_message  - move function print_new_words  - move function print_stage_and_step

--- assistant_with_learning_new_words/game.py
+++ assistant_with_learning_new_words/game.py
@@ -1,14 +1,90 @@
 from random import choice
 
-COLORS = {
-    'red': '\033[31m',
-    'green': '\033[32m',
-    'yellow': '\033[33m',
-    'blue': '\033[34m',
-    'violet': '\033[35m',
-    'light_blue': '\033[36m',
-    'drop_color': '\033[0m'
-}
+
+class Printer:
+    COLORS = {
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'violet': '\033[35m',
+        'light_blue': '\033[36m',
+        'drop_color': '\033[0m'
+    }
+
+    @classmethod
+    def paint(cls, text: str, color: str) -> str:
+        """
+        Функция красит входной текст в указанный цвет.
+
+        Available colors:
+        - 'green'     : Green color
+        - 'red'       : Red color
+        - 'yellow'    : Yellow color
+        - 'blue'      : Blue color
+        - 'violet'    : Violet color
+        - 'light_blue': Light blue color
+
+        :param text: Текст, который нужно раскрасить.
+        :param color: Желаемый цвет текста.
+
+        :return: Входной текст заключен в цветовые коды ANSI для указанного цвета.
+
+        :raises ValueError: Если указан неподдерживаемый цвет.
+        """
+        try:
+            return f"{cls.COLORS[color]}{text}{cls.COLORS['drop_color']}"
+        except KeyError:
+            raise ValueError(f'Нет цвета: {color}')
+
+    @staticmethod
+    def italics(text: str) -> str:
+        """
+        Функция применяет к входному тексту форматирование курсивом.
+
+        :param text: Текст, который будет отформатирован курсивом.
+        :return: Введенный текст с примененным форматированием курсива.
+        """
+        return f"\033[3m{text}\033[0m"
+
+    def print_message(self, result: str, origin: str, translate: str, point: int, color: str) -> None:
+        """
+        Функция печатает текстовый результат с полученными данными.
+
+        :param result: text: Right или Error
+        :param origin: слово на изучаемом языке
+        :param translate: перевод слова на родной язык
+        :param point: доступное количество баллов
+        :param color: цвет для текста
+        :return: None
+        """
+        point = point + 1 if result.lower() == 'right' else point - 1
+
+        print(f'{self.paint(f"{result.title()}! ", color)}'
+              f'{self.italics(f"<< {origin} | {translate} >>")}'
+              f'{self.paint(f" has point: {point}", "green")}\n')
+
+    def print_new_words(self, new_words: dict, zero_indent: int = 0):
+        max_length = len(max(new_words, key=lambda x: len(x[1]))[0])
+
+        third_indent = 20
+        first_indent = third_indent + 13
+        second_indent = max_length + third_indent
+
+        for original, translation in new_words:
+            print(f'{" ":>{zero_indent}}'
+                  f'{self.paint("new pair:", "yellow"):<{first_indent}}'
+                  f'{self.paint(" << ", "yellow")}'
+                  f'{self.paint(f"{self.italics(original):<{second_indent}}", "light_blue")}'
+                  f'{self.paint("|", "yellow"):<{third_indent}}'
+                  f'{self.paint(f"{self.italics(translation)}", "light_blue")}'
+                  f'{self.paint(" >>", "yellow")}'
+                  )
+
+    def print_stage_and_step(self, stage: int, step: int, indent: int = 65):
+        print(f'\n{" ":<{indent}} '
+              f'{self.paint(f"level: {stage}", "violet")}'
+              f'{self.paint(f" | step: {step}", "violet")}\n')
 
 
 class Game:
@@ -68,6 +144,7 @@ class Game:
                 По умолчанию 4.
             stage (int, optional): Количество раз, которое уже прошли по словарю. По умолчанию 1.
         """
+        self.printer = Printer()
         self.some_words = words
         self.words_to_learn = dict()
         self.common_words = dict()
@@ -81,59 +158,6 @@ class Game:
     def __str__(self):
         return self.some_words
 
-    @staticmethod
-    def paint(text: str, color: str) -> str:
-        """
-        Функция красит входной текст в указанный цвет.
-
-        Available colors:
-        - 'green'     : Green color
-        - 'red'       : Red color
-        - 'yellow'    : Yellow color
-        - 'blue'      : Blue color
-        - 'violet'    : Violet color
-        - 'light_blue': Light blue color
-
-        :param text: Текст, который нужно раскрасить.
-        :param color: Желаемый цвет текста.
-
-        :return: Входной текст заключен в цветовые коды ANSI для указанного цвета.
-
-        :raises ValueError: Если указан неподдерживаемый цвет.
-        """
-        try:
-            return f"{COLORS[color]}{text}{COLORS['drop_color']}"
-        except KeyError:
-            raise ValueError(f'Нет цвета: {color}')
-
-    @staticmethod
-    def italics(text: str) -> str:
-        """
-        Функция применяет к входному тексту форматирование курсивом.
-
-        :param text: Текст, который будет отформатирован курсивом.
-        :return: Введенный текст с примененным форматированием курсива.
-        """
-        return f"\033[3m{text}\033[0m"
-
-    @classmethod
-    def print_message(cls, result: str, origin: str, translate: str, point: int, color: str) -> None:
-        """
-        Функция печатает текстовый результат с полученными данными.
-
-        :param result: text: Right или Error
-        :param origin: слово на изучаемом языке
-        :param translate: перевод слова на родной язык
-        :param point: доступное количество баллов
-        :param color: цвет для текста
-        :return: None
-        """
-        point = point + 1 if result.lower() == 'right' else point - 1
-
-        print(f'{cls.paint(f"{result.title()}! ", color)}'
-              f'{cls.italics(f"<< {origin} | {translate} >>")}'
-              f'{cls.paint(f" has point: {point}", "green")}\n')
-
     def read_words(self, words: dict) -> dict:
         """
         Считывает и оценивает вводимые пользователем данные о переводе слов, обновляя словарь слов.
@@ -142,19 +166,15 @@ class Game:
         :return: Обновленный словарь слов после обработки пользовательского ввода.
         """
 
-        print(f'\n{" ":<60} '
-              f'{self.paint(f"level: {self.stage}", "violet")}'
-              f'{self.paint(f" | step: {self.step}", "violet")}\n')
-
         for (original, translation), point in words.items():
-            text = self.paint("Enter translation for", "light_blue")
-            question = input(f'{text} "{self.italics(translation)}": ').strip().lower()
+            text = self.printer.paint("Enter translation for", "light_blue")
+            question = input(f'{text} "{self.printer.italics(translation)}": ').strip().lower()
 
             if question == original.strip().lower():
                 words[(original, translation)] += 1
-                self.print_message("Right", original, translation, point, 'green')
+                self.printer.print_message("Right", original, translation, point, 'green')
             else:
-                self.print_message("Error", original, translation, point, 'red')
+                self.printer.print_message("Error", original, translation, point, 'red')
                 words[(original, translation)] -= 1
 
         return words
@@ -184,17 +204,7 @@ class Game:
         :return: Кортеж, содержащий новую пару слов, где первым элементом является исходное слово или фраза.
                     и второй элемент — это его перевод.
         """
-
-        new_word = choice(self.some_words)
-        print(f'{" ":>35}'
-              f'{self.paint("new pair:", "yellow")}'
-              f'{self.paint(" << ", "yellow")}'
-              f'{self.paint(f"{self.italics(new_word[0]):<30}", "light_blue")}'
-              f'{self.paint("|", "yellow"):<20}'
-              f'{self.paint(f"{self.italics(new_word[1])}", "light_blue")}'
-              f'{self.paint(" >>", "yellow")}')
-
-        return new_word
+        return choice(self.some_words)
 
     def delete_pair(self, pair: tuple) -> None:
         self.some_words.remove(pair)
@@ -210,13 +220,41 @@ class Game:
 
 
 if __name__ == "__main__":
-    second_lesson = [('fault', 'вина'), ('get/receive', 'получать'), ('habit', 'привычка'),
-                     ('disappointment', 'разочарование'), ('jar', 'банка'), ('candle', 'свечка'),
-                     ('brush/comb', 'расческа'), ('hardly ever', 'почти никогда'), ('solve', 'решать(проблемы)'),
-                     ('the same', 'совпадает с'), ('goods', 'продукты'), ('accept', '(принял, подтвердил)')]
+    second_lesson = [('fault', 'вина'), ('get', 'получать(активное действие)'), ('receive', 'получать(пассивное)'),
+                     ('habit', 'привычка'), ('disappointment', 'разочарование'), ('jar', 'банка'), ('candle', 'свечка'),
+                     ('comb', 'расческа'), ('hardly ever', 'почти никогда'), ('solve', 'решать(проблемы)'),
+                     ('the same', 'совпадает с'), ('goods', 'продукты'), ('accept', 'принял, подтвердил')]
+
+    useful_phrases = [('What a pity! Maybe next time.', 'Как жаль! Возможно, в следующий раз.'),
+                      ('I would love to come, but I’m afraid I have to...',
+                       'Я бы с радостью пришёл, но боюсь, что придётся...'),
+                      ('That’s very kind of you!', 'Очень мило с Вашей стороны!'),
+                      ('You really shouldn’t have!', 'Вам действительно не следовало этого делать!'),
+                      ('Bon appetit!', 'Приятного аппетита!'), ('Enjoy your meal!', 'Приятного аппетита!'),
+                      ('Don’t mention it!', 'Не упоминай об этом!'), ('You’re welcome!', 'Пожалуйста!'),
+                      ('It was a pleasure!', 'Это было очень приятно!'), ('My pleasure!', 'Не за что!'),
+                      ('Congrats!', 'Поздравляю!'), ('Congratulations!', 'Поздравляем!'),
+                      ('Sorry I’m late!', 'Извините, я опоздал!'), ('Sorry for being late!', 'Извините за опоздание!'),
+                      ('Sorry! I got stuck in a traffic jam.', 'Извини! Я застрял в пробке.'),
+                      ('Give/Pass my regards to...', 'Передайте/передайте привет...'),
+                      ('Remember me to...', 'Запомни меня, чтобы...'), ('Happy birthday!', 'С днем рождения!'),
+                      ('Many happy returns!', 'Много счастливых возвращений!'),
+                      ('That would be nice, thank you.', 'Было бы здорово, спасибо.'),
+                      ('That’s very kind of you.', 'Очень мило с Вашей стороны.'),
+                      ('Merry Christmas!', 'С Рождеством!'),
+                      ('Happy Christmas!', 'Счастливого Рождества!'), ('Bless you!', 'Будьте здоровы!'),
+                      ('I hope you feel better soon.', 'Надеюсь, вам вскоре будет лучше.'),
+                      ('I hope you get better soon.', 'Надеюсь, ты скоро поправишься.'),
+                      ('Bye, see you soon!', 'Пока, скоро увидимся!'), ('See you later!', 'Увидимся позже!'),
+                      ('Have a good trip!', 'Удачной поездки!'), ('Have a nice flight!', 'Приятного полета!'),
+                      ('Have a nice holiday!', 'Хороших праздников!'),
+                      ('Could you pass the salt, please?', 'Не могли бы вы передать соль, пожалуйста?'),
+                      ('May I smoke?', 'Можно я закурю?'), ('Do you mind if I smoke?', 'Ты не против если я покурю?')]
 
     game = Game(second_lesson)
     game.words_to_learn = game.start_game()
+    game.printer.print_new_words(game.words_to_learn, 30)
+    game.printer.print_stage_and_step(stage=game.stage, step=game.step)
 
     while game.stage < game.repeat:
         game.words_to_learn = game.read_words(game.words_to_learn)
