3f4ab7332f75c7429a358fd7bb12c079742d69d4
Stanislav <nstanislass@gmail.com>
2023-09-20 14:03:42 +0300

    add info about combinations_with_replacement

--- libraries/itertools _module/itertools_combinations_with_replacement.py
+++ libraries/itertools _module/itertools_combinations_with_replacement.py
@@ -1,8 +1,72 @@
-# This tool returns r length subsequences of elements from the input iterable allowing individual elements
-# to be repeated more than once.
+# Функция `combinations_with_replacement` из модуля `itertools` в Python создает итератор, который
+# генерирует все возможные комбинации элементов из входной последовательности с повторениями, заданной длины.
 #
-# Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted,
-# the combination tuples will be produced in sorted order.
+# Синтаксис функции `combinations_with_replacement` следующий:
+
+# itertools.combinations_with_replacement(iterable, r)
+
+# - `iterable`: Это исходная последовательность (например, строка, список или кортеж),
+# из которой будут генерироваться комбинации с повторениями.
+#
+# - `r` (необязательный): Этот параметр определяет длину комбинаций. Если он не указан,
+# то по умолчанию используется длина исходной последовательности. Если `r` указан,
+# то будут сгенерированы только комбинации заданной длины.
+#
+# Функция возвращает итератор, который вы можете пройти в цикле `for` или передать в другие функции,
+# такие как `list()` для получения списка всех комбинаций.
+#
+# Примеры:
+# 1. Генерация всех комбинаций с повторениями из трех элементов:
+# from itertools import combinations_with_replacement
+#
+# elements = [1, 2, 3]
+# combinations_result = combinations_with_replacement(elements, 2)
+#
+# for comb in combinations_result:
+#     print(comb)
+
+# Вывод:
+# (1, 1)
+# (1, 2)
+# (1, 3)
+# (2, 2)
+# (2, 3)
+# (3, 3)
+#
+# 2. Генерация всех комбинаций с повторениями из четырех элементов:
+# from itertools import combinations_with_replacement
+#
+# elements = [1, 2, 3, 4]
+# combinations_result = combinations_with_replacement(elements, 3)
+#
+# for comb in combinations_result:
+#     print(comb)
+#
+# Вывод:
+# (1, 1, 1)
+# (1, 1, 2)
+# (1, 1, 3)
+# (1, 1, 4)
+# (1, 2, 2)
+# (1, 2, 3)
+# (1, 2, 4)
+# (1, 3, 3)
+# (1, 3, 4)
+# (1, 4, 4)
+# (2, 2, 2)
+# (2, 2, 3)
+# (2, 2, 4)
+# (2, 3, 3)
+# (2, 3, 4)
+# (2, 4, 4)
+# (3, 3, 3)
+# (3, 3, 4)
+# (3, 4, 4)
+# (4, 4, 4)
+#
+# Функция `combinations_with_replacement` полезна, когда вам нужно создать комбинации элементов
+# с возможностью повторений, например, при генерации всех возможных наборов элементов для задач,
+# связанных с выбором из множества с возвращением.
 
 # Task
 # You are given a string S.
