0cff1d0428f8c379555dbf0e211a5bb5ea4ba82e
Stanislav <nstanislass@gmail.com>
2023-09-20 13:59:13 +0300

    add info about combinations

--- libraries/itertools _module/itertools_combinations.py
+++ libraries/itertools _module/itertools_combinations.py
@@ -1,7 +1,52 @@
-# This tool returns the r length subsequences of elements from the input iterable.
+# Функция `combinations` из модуля `itertools` в Python создает итератор, который генерирует все возможные комбинации
+# элементов из входной последовательности без повторений, заданной длины.
 #
-# Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted,
-# the combination tuples will be produced in sorted order.
+# Синтаксис функции `combinations` следующий:
+
+# itertools.combinations(iterable, r)
+
+# - `iterable`: Это исходная последовательность (например, строка, список или кортеж),
+# из которой будут генерироваться комбинации.
+#
+# - `r` (необязательный): Этот параметр определяет длину комбинаций.
+# Если он не указан, то по умолчанию используется длина исходной последовательности.
+# Если `r` указан, то будут сгенерированы только комбинации заданной длины.
+#
+# Функция возвращает итератор, который вы можете пройти в цикле `for` или передать в другие функции,
+# такие как `list()` для получения списка всех комбинаций.
+#
+# Примеры:
+# 1. Генерация всех комбинаций из трех элементов:
+# from itertools import combinations
+#
+# elements = [1, 2, 3]
+# combinations_result = combinations(elements, 2)
+#
+# for comb in combinations_result:
+#     print(comb)
+#
+# Вывод:
+# (1, 2)
+# (1, 3)
+# (2, 3)
+#
+# 2. Генерация всех комбинаций из четырех элементов:
+# from itertools import combinations
+#
+# elements = [1, 2, 3, 4]
+# combinations_result = combinations(elements, 3)
+#
+# for comb in combinations_result:
+#     print(comb)
+
+# Вывод:
+# (1, 2, 3)
+# (1, 2, 4)
+# (1, 3, 4)
+# (2, 3, 4)
+#
+# Функция `combinations` полезна, когда вам нужно создать комбинации элементов для решения задач,
+# связанных с выбором наборов элементов из некоторого множества.
 
 # Task
 #
