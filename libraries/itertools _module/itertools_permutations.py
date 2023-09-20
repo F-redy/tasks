4d9f9012e95433564d84d40cc46749eb0c93b267
Stanislav <nstanislass@gmail.com>
2023-09-20 13:55:12 +0300

    add info about permutations

--- libraries/itertools _module/itertools_permutations.py
+++ libraries/itertools _module/itertools_permutations.py
@@ -1,10 +1,56 @@
-# This tool returns successive  length permutations of elements in an iterable.
+# Функция `permutations` из модуля `itertools` в Python создает итератор, который генерирует все возможные
+# перестановки элементов из входной последовательности (с повторениями или без, в зависимости от параметров)
+# в определенной длине.
 #
-# If  is not specified or is None, then  defaults to the length of the iterable,
-# and all possible full length permutations are generated.
+# Синтаксис функции `permutations` следующий:
 #
-# Permutations are printed in a lexicographic sorted order.
-# So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
+# itertools.permutations(iterable, r)
+#
+# - `iterable`: Это исходная последовательность (например, строка, список или кортеж),
+# из которой будут генерироваться перестановки.
+#
+# - `r` (необязательный): Этот параметр определяет длину перестановок. Если он не указан,
+# то по умолчанию используется длина исходной последовательности.
+# Если `r` указан, то будут сгенерированы только перестановки заданной длины.
+#
+# Функция возвращает итератор, который вы можете пройти в цикле `for` или передать в другие функции,
+# такие как `list()` для получения списка всех перестановок.
+#
+# Примеры:
+#
+# 1. Генерация всех перестановок из трех элементов:
+# from itertools import permutations
+#
+# elements = [1, 2, 3]
+# permutations_result = permutations(elements)
+#
+# for perm in permutations_result:
+#     print(perm)
+#
+# Вывод:
+# (1, 2, 3)
+# (1, 3, 2)
+# (2, 1, 3)
+# (2, 3, 1)
+# (3, 1, 2)
+# (3, 2, 1)
+#
+# 2. Генерация всех перестановок из двух элементов:
+# from itertools import permutations
+#
+# elements = [1, 2]
+# permutations_result = permutations(elements, 2)
+#
+# for perm in permutations_result:
+#     print(perm)
+#
+# Вывод:
+# (1, 2)
+# (2, 1)
+#
+# Функция `permutations` полезна, когда вам нужно исследовать все возможные комбинации
+# элементов в заданной последовательности.
+
 # Task
 #
 # You are given a string S.
