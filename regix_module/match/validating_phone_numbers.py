141fe321d59ecd56c978b6147eba3f9b90d32ad8
Stanislav <nstanislass@gmail.com>
2023-09-21 15:45:52 +0300

    add validating_phone_numbers.py

---
+++ regix_module/match/validating_phone_numbers.py
@@ -0,0 +1,34 @@
+# Let's dive into the interesting topic of regular expressions! You are given some input,
+# and you are required to check whether they are valid mobile numbers.
+#
+# A valid mobile number is a ten digit number starting with a 7, 8 or 9.
+#
+# Concept
+#
+# A valid mobile number is a ten digit number starting with a 7, 8 or 9.
+#
+# #Input Format
+#
+# The first line contains an integer N, the number of inputs.
+# N lines follow, each containing some string.
+#
+# Output Format
+#
+# For every string listed, print "YES" if it is a valid mobile number and "NO" if it is not on separate lines.
+# Do not print the quotes.
+#
+# Sample Input
+# 2
+# 9587456281
+# 1252478965
+#
+# Sample Output
+# YES
+# NO
+
+import re
+
+pattern = re.compile(r"[789][0-9]{9}")
+
+for _ in range(int(input())):
+    print('YES' if pattern.match(input()) else 'NO')
