568f6e358155ada20ca58040087471d789196789
Stanislav <nstanislass@gmail.com>
2023-09-24 22:33:00 +0300

    delete generation_numbers generator_phones_numbers

--- regix_module/fullmatch/check_ua_phone_number.py
+++ regix_module/fullmatch/check_ua_phone_number.py
@@ -1,7 +1,6 @@
 import re
-from random import randint
 
-from typing_extensions import Generator
+from function_generators.phone_number_generator import generator_phones_numbers, codes_operators, codes_county
 
 MIN_PHONE_LENGTH = 7
 MAX_PHONE_LENGTH = 13
@@ -12,30 +11,6 @@ PERMITTED_OPERATORS = {
     "Мошеннические": ("070", "080", "090", "056", "057"),
     "Тримоб, PeopleNet, Интертелеком": ("091", "092", "094")
 }
-
-code_regions = {'Винница': '043', 'Днепр': '056', 'Донецк': '062', 'Житомир': '041', 'Запорожье': '061',
-                'Ивано-Франковск': '034', 'Киев': '044 и 045', 'Кропивницкий': '052', 'Луганск': '064', 'Луцк': '033',
-                'Львов': '032', 'Николаев': '051', 'Одесса': '048', 'Полтава': '053', 'Ровно': '036', 'Сумы': '054',
-                'Тернополь': '035', 'Ужгород': '031', 'Харьков': '057', 'Херсон': '055', 'Хмельницкий': '038',
-                'Черкассы': '047', 'Чернигов': '046', 'Черновцы': '037'}
-
-codes_operators = ['067', '096', '097', '098', '050', '066', '095', '099', '063', '073', '093', '070',
-                   '080', '090', '056', '057', '091', '092', '094']
-
-codes_county = ["+38", "38", "8"]
-
-
-def generation_numbers(length_number: int):
-    return "".join(str(randint(0, 9)) for _ in range(length_number))
-
-
-def generation_phones_numbers(operators: list[str], codes: list[str], count_phones: int) -> Generator[str, None, None]:
-    for _ in range(count_phones):
-        random_code = codes[randint(0, len(codes) - 1)]
-        operator = operators[randint(0, len(operators) - 1)]
-        number = generation_numbers(length_number=7)
-
-        yield random_code + operator + number
 
 
 def get_name_operator(code_operator: str) -> str:
@@ -87,12 +62,7 @@ def validate_ukrainian_phone_number(phone_number: str) -> str:
 
 
 if __name__ == '__main__':
+    ua_phone_numbers = generator_phones_numbers(codes_operators, codes_county, count_phones=100)
 
-    random_operators = ["".join(str(randint(0, 9)) for _ in range(3)) for _ in range(25)]
-    random_code_country = ["".join(str(randint(0, 9)) for _ in range(randint(1, 2))) for _ in range(5)]
-
-    test_numbers = generation_phones_numbers(codes_operators, codes_county, 100)
-    test_random_numbers = generation_phones_numbers(random_operators, random_code_country, 200)
-
-    for n in test_numbers:
+    for n in ua_phone_numbers:
         print(validate_ukrainian_phone_number(n))
