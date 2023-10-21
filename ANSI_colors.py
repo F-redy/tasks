944310e00e8f3603d738ab8c77e286774d16f44a
Stanislav <nstanislass@gmail.com>
2023-10-21 16:38:31 +0300

    add ANSI colors dictionary

---
+++ ANSI_colors.py
@@ -0,0 +1,9 @@
+COLORS = {
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'violet': '\033[35m',
+    'light_blue': '\033[36m',
+    'drop_color': '\033[0m'
+}
